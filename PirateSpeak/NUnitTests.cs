6e3c1273f1ec9ad26061222f1b27718381fc6d37
Scott Schaffer <scottpschaffer@hotmail.com>
2017-02-05 15:19:06 -0800

    Added NUnit tests

---
+++ PirateSpeak/NUnitTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace PirateSpeak
+{
+    [TestFixture]
+    public class NUnitTests
+    {
+        [TestCase("trisf", new[] { "first" }, new[] { "first" })]
+        [TestCase("oob", new[] { "bob", "baobob" }, new string[0])]
+        [TestCase("ainstuomn", new[] { "mountains", "hills", "mesa" }, new[] { "mountains" })]
+        [TestCase("oopl", new[] { "donkey", "pool", "horse", "loop" }, new[] { "pool", "loop" })]
+        [TestCase("oprst", new[] { "sport", "ports", "ball", "bat", "port" }, new[] { "sport", "ports" })]
+        public void TestPirateVocabulary(string jumble, string[] dictionary, object expectedResult)
+        {
+            var actualResult = new Solution().GetPossibleWords(jumble, dictionary);
+            Assert.AreEqual(actualResult, expectedResult);
+        }
+
+    }
+}
